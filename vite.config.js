ebac7f6ff29286609cc2e7d9cabeea7e867f8a8d
iamgillespie <gailen.gillespie@gmail.com>
2024-03-08 16:38:42 -0600

    adding vue.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-/////////////////////////////////////////////////////////////////////
-  // vue.config.js file to be placed in the root of your repository
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/retroscan/'
-    : '/'
-}
-/////////////////////////////////////////////////////////////////////
-
 // https://vitejs.dev/config/
 export default defineConfig({
-
-
-
   plugins: [
     vue(),
   ],
