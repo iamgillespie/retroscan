ba70fd69268c077b458252dc2891d56dbc92f179
iamgillespie <gailen.gillespie@gmail.com>
2024-03-08 16:34:37 -0600

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+/////////////////////////////////////////////////////////////////////
+  // vue.config.js file to be placed in the root of your repository
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/retroscan/'
+    : '/'
+}
+/////////////////////////////////////////////////////////////////////
+
 // https://vitejs.dev/config/
 export default defineConfig({
+
+
+
   plugins: [
     vue(),
   ],
@@ -16,9 +28,3 @@ export default defineConfig({
 })
 
 
-// vue.config.js file to be placed in the root of your repository
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/retroscan/'
-    : '/'
-}
