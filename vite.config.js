b6e76cbbc1cea34a8246b6e1eca79991a2ec71d8
iamgillespie <gailen.gillespie@gmail.com>
2024-03-08 16:12:20 -0600

    adding cinfig files

--- vite.config.js
+++ vite.config.js
@@ -14,3 +14,11 @@ export default defineConfig({
     }
   }
 })
+
+
+// vue.config.js file to be placed in the root of your repository
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/retroscan/'
+    : '/'
+}
